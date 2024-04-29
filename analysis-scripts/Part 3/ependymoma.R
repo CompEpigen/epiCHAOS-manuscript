dd33ac9015e395a4d2207e0180326d7db5699286
Kelly, Katherine <katherine.kelly@dkfz.de>
2024-04-29 14:05:04 +0200

    tidy cancer analysis scripts

--- analysis-scripts/Part 3/ependymoma.R
+++ analysis-scripts/Part 3/ependymoma.R
@@ -11,67 +11,68 @@ library(stringr)
 set.seed(10)
 
 data.dir <- "/omics/groups/OE0219/internal/KatherineK/data/scATAC/Ependymoma/"
-analysis.dir <- 
 
 #--- scATAC from ependymoma
 epn <- readRDS(file.path(data.dir, "GSE206579_snATAC_PF_EPN_Seurat.rds"))
 
-# check umap
+#--- check umap
 DimPlot(epn)
-DimPlot(epn, split.by = "biological_replicates")
 
-# compute eITH per cluster
-epn@meta.data$celltype <- Idents(epn)
-
-epn@meta.data$group <- paste0(epn@meta.data$celltype, "-", epn@meta.data$biological_replicates)
-epn@meta.data$group <- epn@meta.data$celltype
-
+#--- set group for epiCHAOS calculation
+epn@meta.data$group <- epn@meta.data$celltype <- Idents(epn)
 epn@meta.data$group %>% table()
 
+#--- peaks-by-cells matrix
 atac <- epn@assays$ATAC@counts %>% na.omit()
-
 dim(atac)
 
-# populate list of datasets for each cell-type-sample pair
+#--- populate list of peaks-by-cells matrices for each cell type
 datasets <- list()
 for (i in unique(epn$group)) {
+  
+  #--- cell ids for the selected cell type
   ids <- epn@meta.data[epn$group==i,] %>% rownames()
+  
+  #--- skip if too few cells, subset to 200 if many cells
   if (length(ids)<20) { next }
   temp <- atac[,ids]
   if (ncol(temp)>200) { temp <- temp[,sample(ncol(temp), 200)]}
+  
+  #--- adjust rownames
   rownames(temp) <- rownames(temp) %>% str_replace("-", ":")
+  
+  #--- binarise
   temp[temp>1] <- 1
+  
   datasets[[i]] <- temp
 }
 
 lapply(datasets, dim)
 
 
-# compute eITH
+#--- compute epiCHAOS scores per celltype
 het <- compute.eITH(datasets)
 het <- compute.eITH.cancer(datasets)
-#het$state <- het$state %>% str_replace("_", " ")
+#het %>% write.csv("/omics/groups/OE0219/internal/KatherineK/ATACseq/epiCHAOS-supplementary-data/epiCHAOS_ependymoma.csv")
 
-#--- check associations with metastasis
-epn@meta.data$origin <- ifelse(epn@meta.data$biological_replicates %in% c("M7", "M8"), "metastasis", "primary")
-
-DimPlot(epn, group.by = "origin")
-DimPlot(epn, group.by = "celltype")
-
-# calculate average eITH score per population
+#--- add epiCHAOS scores to epn metadata
 for (i in unique(het$state)) {
   epn@meta.data$epiCHAOS[epn@meta.data$celltype==i] <- het$mean.het[het$state==i]
 }
 
-
+#--- adjust labels for plotting
 het$malignant <- ifelse(het$state %in% c("Undifferentiated_cells", "NPCs", "Ependymal_cells", "MLCs", "Astrocytes"), "malignant", "non-malignant")
 het$state <- het$state %>% str_replace_all("_", " ")
 Idents(epn) <- epn@meta.data$celltype %>% str_replace_all("_", " ")
 
 library(RColorBrewer)
 library(ggpubr)
+
+#--- umaps and barplots of epiCHAOS scores per-celltype
 p1 <- DimPlot(epn) + scale_color_brewer(palette = "Set3") +labs(x="UMAP1", y="UMAP2")
+
 p2 <- FeaturePlot(epn, features="epiCHAOS")  + labs(x="UMAP1", y="UMAP2") + scale_color_distiller(palette = "Blues", direction = 1)
+
 p3 <- ggplot(temp, aes(x=reorder(state, mean.het), y=mean.het, fill=malignant)) +
   geom_bar(position="dodge", stat = "identity", alpha=0.9, width = 0.6) +
   scale_fill_manual(values = c("steelblue4", "grey20"))+
@@ -81,46 +82,4 @@ p3 <- ggplot(temp, aes(x=reorder(state, mean.het), y=mean.het, fill=malignant)) 
 
 
 ggarrange(p1,p2,p3, ncol=3, widths = c(1, 0.8, 0.8))
-
-
-#--- try clustering the cells and performing eITH calculation on the clusters
-
-epn <- FindNeighbors(object = epn, reduction = 'lsi', dims = 2:20)
-epn <- FindClusters(object = epn, verbose = FALSE, algorithm = 3)
-DimPlot(object = epn, label = TRUE) + NoLegend()
-epn@meta.data$seurat_clusters
-
-datasets <- list()
-for (i in unique(epn$seurat_clusters)) {
-  ids <- epn@meta.data[epn$seurat_clusters==i,] %>% rownames()
-  if (length(ids)<20) { next }
-  temp <- atac[,ids]
-  if (ncol(temp)>100) { temp <- temp[,sample(ncol(temp), 100)]}
-  datasets[[i]] <- temp
-}
-
-lapply(datasets, dim)
-names(datasets) <- paste0("cluster ", names(datasets))
-
-# compute eITH
-het <- compute.eITH(datasets)
-
-
-# add eITH score to sample annotation
-for (i in unique(epn$seurat_clusters)) {
-  epn@meta.data$eICH[epn@meta.data$seurat_clusters==i] <- het$mean.het[str_remove(het$state, "cluster ")==i]
-}
-
-p1 <- DimPlot(epn, group.by = "celltype", label = T) + NoLegend() + labs(x="UMAP 1", y = "UMAP 2")
-p2 <- FeaturePlot(epn, features = "eICH") + 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 9, name = "RdBu"))) + labs(x="UMAP 1", y = "UMAP 2")
-
-pdf("ependymoma/umaps_epn_celltype_vs_eICH.pdf", 9, 4)
-ggarrange(p1,p2, widths = c(4,5))
-dev.off()
-
-
-Idents(epn) <- epn$celltype
-FeaturePlot(epn, features = "eICH", split.by = "origin", label=T) &
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdBu")))
 
--- analysis-scripts/Part 3/gene_set_scores.R
+++ analysis-scripts/Part 3/gene_set_scores.R
@@ -26,67 +26,82 @@ rownames(mat.ge) <- row.names
 dim(mat.ge)
 mat.ge[1:10,1:10]
 
-# get cluster annotation from metadata
+#--- get cluster annotation from metadata
 meta <- lica@cellColData[,c("Sample", "Clusters")] %>% data.frame()
 
-# msigdbr gene sets
+#--- msigdbr gene sets
 gene_sets = msigdbr(species = "Homo sapiens")
 head(gene_sets)
 
 set.names <- gene_sets$gs_name %>% unique()
 length(set.names)
 
-# malignant cell clusters
+#--- malignant cell clusters
 clusters <- unique(lica$Clusters)
 
 #--- compute per cluster scores per gene set
 gene.set.scores <- data.frame(row.names = clusters)
 
-#--- for each gene set, retrieve the list of genes within that set, 
+#--- for each gene set, retrieve the list of genes within that set, and compute the average gene scores and gene-set scores for all cells within each cluster
 for (set in set.names) {
-  print(set)
-  genes <- gene_sets$gene_symbol[gene_sets$gs_name==set] %>% intersect(rownames(mat.ge)) # retrieve genes in the selected gene set, of which correspond to rownames of gene score matrix
-  if (length(genes)<10) { next } # exclude sets with fewer than 10 genes
-  mat.set <- mat.ge[rownames(mat.ge) %in% genes,]
-  temp <- t(mat.set) %>% as.matrix() %>% data.frame() # transpose subset gene scores matrix and coerce to dataframe
-  temp <- merge(temp, meta, by=0) %>% keepRow() # merge gene scores and cluster annotation
   
-  # compute average gene scores for all cells within each
+  print(set)
+  
+  #--- retrieve genes in the selected gene set, of which correspond to rownames of gene score matrix
+  genes <- gene_sets$gene_symbol[gene_sets$gs_name==set] %>% intersect(rownames(mat.ge)) 
+  
+  #--- exclude sets with fewer than 10 genes
+  if (length(genes)<10) { next } 
+  
+  #--- subset for genes in set of interest
+  mat.set <- mat.ge[rownames(mat.ge) %in% genes,]
+  
+  #--- transpose subset gene scores matrix and coerce to dataframe
+  temp <- t(mat.set) %>% as.matrix() %>% data.frame() 
+  
+  #--- merge gene scores and cluster annotation
+  temp <- merge(temp, meta, by=0) %>% keepRow() 
+  
+  #--- compute average gene scores for all cells within each
   gex.per.cluster <- data.frame()
   for (i in clusters) {
     gex.per.cluster <- rbind(gex.per.cluster, colMeans(temp[temp$Clusters==i,colnames(temp) %notin% c("Sample", "Clusters")]))
   }
   
-  # set rownames to cluster names and colnames to gene names
+  #--- set rownames to cluster names and colnames to gene names
   rownames(gex.per.cluster) <- clusters
   colnames(gex.per.cluster) <- colnames(temp[,colnames(temp) %notin% c("Sample", "Clusters")])
   
-  # compute average of gene scores for each cluster to produce the gene set scores per cluster
+  #--- compute average of gene scores for each cluster to produce the gene set scores per cluster
   gene.set.scores[,set] <- rowMeans(gex.per.cluster)
   
 }
 
-#--- load epigenetic heterogeneity scores
+#--- load previously computed epiCHAOS scores
 het <- readRDS("epiCHAOS_scores_copy_corrected_all_peaks.Rds")
 
+#--- merge gene set scores and epiCHAOS scores
 temp <- merge(gene.set.scores, het[,c("state", "mean.het")], by.x=0, by.y="state") %>% unique()
 rownames(temp) <- temp$Row.names
 temp$Row.names <- NULL
+
+#--- compute correlations between each gene set score and epiCHAOS score
 cor <- cor(as.matrix(temp))
 cor["mean.het",] %>% sort()
 
-# save list contianing the pathway scores and their correlations with epiCHAOS
+#--- save list contianing the gene set scores and their correlations with epiCHAOS
 obj <- list(scores=temp, correlation=cor)
-
 saveRDS(obj, file = "gene_set_scores_vs_het_allgenesets.Rds")
 
 #--- downstream analysis
 obj <- readRDS("gene_set_scores_vs_het_allgenesets.Rds")
 
+#--- scores and correlations
 scores <- obj$scores
 cor <- obj$correlation
 cor <- cor[,"mean.het"]
 
+#--- for plotting
 temp <- data.frame(cor)
 colnames(temp) <- "pearson"
 temp$n <- 1:nrow(temp)
@@ -95,7 +110,7 @@ labels <- c("HALLMARK_EPITHELIAL_MESENCHYMAL_TRANSITION")
 temp$label <- ifelse(rownames(temp) %in% labels, rownames(temp), "")
 temp$label <- temp$label %>% str_replace_all("_", " ")
 
-# plot of correlations
+#--- Figure 3B-C. plot of correlations
 ggplot(temp, aes(y=pearson, x=reorder(n, pearson), label=label)) +
   geom_point(size=1, alpha=0.7)+
   labs(x="", y="Correlation vs epiCHAOS") +
@@ -103,11 +118,10 @@ ggplot(temp, aes(y=pearson, x=reorder(n, pearson), label=label)) +
   theme_classic() +
   theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
 
-# check top 5
+#--- check top 5
 temp %>% arrange(pearson) %>% tail() %>% rownames() %>% rev()
 
-
-# correlation plot of interesting gene sets in liver cancer
+#--- correlation plot of interesting gene sets in liver cancer
 interest <- c("MEBARKI_HCC_PROGENITOR_WNT_UP", "CHIANG_LIVER_CANCER_SUBCLASS_CTNNB1_UP", "HOSHIDA_LIVER_CANCER_SURVIVAL_UP")
 
 cor <- obj$correlation
